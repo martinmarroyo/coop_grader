9915752e1f0cadb4c786afaf9be9eb09c29c1747
Martin Arroyo <marroyo7775@gmail.com>
2022-12-02 23:33:44 -0500

    Patched issue with setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import coop_grader
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'coop_grader',
     version = coop_grader.__version__,
     author = 'Martin Arroyo',
     author_email = 'captmarroyo@coopcareers.org',
-    packages = ['coop_grader'],
+    packages = find_packages(),
     install_requires = ['pyyaml']
 )

ea5470d1ec942b980af44a67d8f36ba98a76a89a
Martin Arroyo <marroyo7775@gmail.com>
2022-12-03 00:06:54 -0500

    Updated testrunner to enable consuming local yaml file

--- coop_grader/python101/testrunner.py
+++ coop_grader/python101/testrunner.py
@@ -1,10 +1,19 @@
+import pkg_resources
+import yaml
+from yaml.loader import SafeLoader
 from coop_grader.tools import binder, core
 from pathlib import Path
 
+
 def validator(conf_filename: str = 'questions.yaml') -> dict:
-    path = r'coop_grader\python101\testrunner.py'
-    validator_funcs = core.generate_tests(filepath=path)
-    return validator_funcs
+    try:
+        file = pkg_resources.resource_stream(__name__, conf_filename)
+        conf = yaml.load(file, Loader=SafeLoader)
+        validator_funcs = core.generate_tests(conf=conf)
+    except FileNotFoundError:
+        print('Error: Did you write your answer sheet to the `questions.yaml` file for this lesson?')
+    else:
+        return validator_funcs
 
     
 def final_check():

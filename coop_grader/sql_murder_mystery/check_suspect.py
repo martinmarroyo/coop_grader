589ef790783bdef738e51eaf688885f45ea5cd3c
Martin Arroyo <marroyo7775@gmail.com>
2023-08-15 17:22:57 -0400

    Added SQL Murder Mystery checks

---
+++ coop_grader/sql_murder_mystery/check_suspect.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+
+def check_suspect(suspect):
+    guess = suspect.lower()
+    murderer = "jeremy bowers"
+    mastermind = "miranda priestly"
+    if guess not in (murderer, mastermind):
+        pprint(f"{suspect} is not the murderer! Keep on investigating, sleuth!")
+        return False
+    if guess == murderer:
+        pprint("""
+            Congrats, you found the murderer! But wait, there's more... If you think you're up for a challenge, 
+            try querying the interview transcript of the murderer to find the real villain behind this crime. 
+            If you feel especially confident in your SQL skills, try to complete this final step with no more than 2 queries. 
+            
+            Use this same `check_suspect` function with your new suspect to check your answer.
+        """)
+        return True
+    if guess == mastermind:
+        pprint("""
+              Congrats, you found the brains behind the murder! 
+              
+              Everyone in SQL City hails you as the greatest SQL detective of all time. 
+              
+              Time to break out the champagne!
+        """)
+        return True

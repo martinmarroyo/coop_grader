9ad6930f40db7562faa1e7b4d498a6bcab234412
Martin Arroyo <marroyo7775@gmail.com>
2023-08-15 17:50:44 -0400

    Updated formatting for SQL Murder Mystery

--- coop_grader/sql_murder_mystery/check_suspect.py
+++ coop_grader/sql_murder_mystery/check_suspect.py
@@ -6,20 +6,17 @@ def check_suspect(suspect):
         print(f"{suspect} is not the murderer! Keep on investigating, sleuth!")
         return False
     if guess == murderer:
-        print("""
-            Congrats, you found the murderer! But wait, there's more... If you think you're up for a challenge, 
-            try querying the interview transcript of the murderer to find the real villain behind this crime. 
-            If you feel especially confident in your SQL skills, try to complete this final step with no more than 2 queries. 
-            
-            Use this same `check_suspect` function with your new suspect to check your answer.
-        """)
+        print(
+            "Congrats, you found the murderer! But wait, there's more... If you think you're up for a challenge," 
+            "try querying the interview transcript of the murderer to find the real villain behind this crime." 
+            "If you feel especially confident in your SQL skills, try to complete this final step with no more than 2 queries.\n"     
+            "Use this same `check_suspect` function with your new suspect to check your answer."
+        )
         return True
     if guess == mastermind:
-        print("""
-              Congrats, you found the brains behind the murder! 
-              
-              Everyone in SQL City hails you as the greatest SQL detective of all time. 
-              
-              Time to break out the champagne!
-        """)
+        print(
+            "Congrats, you found the brains behind the murder!\n" 
+            "Everyone in SQL City hails you as the greatest SQL detective of all time.\n" 
+            "Time to break out the champagne!"
+        )
         return True
